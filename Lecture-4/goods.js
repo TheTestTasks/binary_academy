8e50408e1dfba17191b94f49e1129a55bed36297
brevis <brevis.ua@gmail.com>
2015-06-10 14:58:57 +0300

    init

---
+++ Lecture-4/goods.js
@@ -0,0 +1,90 @@
+$(function() {
+
+    // Add new goods / edit existing goods handler
+    $(document).on('keypress', function(e) {        
+        var $target = $(e.target);
+
+        if (!$target.hasClass('edit-goods') && !$target.hasClass('new-goods')) return;
+
+        var title = $.trim($target.val());
+        if (e.keyCode != 13 || title == '') return;
+
+        if ($target.hasClass('edit-goods')) {
+            $target.parent().find('.view-goods label').text($target.val());
+            closeEditBox();
+        } else {
+            var tpl = $('#goods-item-tpl').html();
+            title = $('<div />').text(title).html(); // escape title jQuery way :)
+            tpl = tpl.replace(/\{\{title\}\}/g, title);
+            $('ul.goods-list').append(tpl);
+            $target.val('');
+            refreshGoodsMark();
+        }        
+    }).on('keyup', function(e) {
+        if (e.keyCode != 27) return;
+
+        var $target = $(e.target);
+        if (!$target.hasClass('edit-goods') && !$target.hasClass('new-goods')) return;
+
+        if ($target.hasClass('edit-goods')) {
+            $target.val( $target.parent().find('.view-goods label').text() );
+            closeEditBox();
+        } else {
+            $target.val('');
+        }
+    });
+
+    // Delete goods handler
+    $(document).on('click', '.delete-goods, .delete-all-goods', function() {
+        if ($(this).hasClass('delete-all-goods')) {
+            $('.mark-goods.mark-single:checked').each(function(){
+                $(this).parent().parent().remove();    
+            });
+        } else {
+            $(this).parent().parent().remove();    
+        }        
+        refreshGoodsMark();
+        $('input.new-goods').focus();
+    });
+
+    // Show exit goods box (double click handler)
+    $(document).on('dblclick', '.view-goods label', function() {
+        $(this).parent().hide();
+        $(this).parent().parent().find('.edit-goods').show().focus();
+    });
+
+    // Mark goods handler
+    $(document).on('change', '.mark-goods', function() {
+        if ($(this).hasClass('mark-all')) {
+            $('.mark-goods.mark-single').each(function(){
+                markGoods($(this), $('.mark-all').prop('checked'));
+            });            
+        } else {
+            markGoods($(this), $(this).prop('checked'));            
+        }
+        refreshGoodsMark();
+    });
+
+    // Catch click on any place of page for close edit goods box
+    $(document).on('click', function(e) {
+        if (!$(e.target).hasClass('edit-goods')) closeEditBox();        
+    });
+
+    
+    // Helpers
+    function closeEditBox() {
+        $('.goods-list li .edit-goods').hide();
+        $('.goods-list li .view-goods').show();
+        $('input.new-goods').focus();
+    }
+
+    function markGoods($el, checked) {
+        $el.parent().parent().toggleClass('marked');
+        $el.prop('checked', checked);
+    }
+
+    function refreshGoodsMark() {
+        $('.mark-all').prop('checked', $('.goods-list li').length > 0 && ($('.mark-single:checked').length == $('.goods-list li').length));
+        $('.delete-all-goods').prop('disabled', $('.mark-goods.mark-single:checked').length == 0);        
+    }
+});
---
+++ Lecture-4/readme.md
@@ -0,0 +1,5 @@
+Выполнить задания описанные в данном [файле](https://docs.google.com/document/d/176KJjpd2aHX50QAVZcJJilVIPX9A7i0_mTYGBosy1i0/edit?usp=sharing). На картинках изображен приблизительный внешний вид полученного приложения.
+
+----------
+
+[Live demo](https://dl.dropboxusercontent.com/u/2073681/goods/goods.html)
